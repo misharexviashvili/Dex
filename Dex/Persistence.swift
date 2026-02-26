1fb729d96d56a447c0e7d85397db6f43bf2b2bae
Mikhaeli <Mihaelirekhviashvili@gmail.com>
2026-02-26 17:06:09 +0400

    codingkeys

--- Dex/Persistence.swift
+++ Dex/Persistence.swift
@@ -45,9 +45,10 @@ struct PersistenceController {
         }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+                print(error)
             }
         })
+        container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
