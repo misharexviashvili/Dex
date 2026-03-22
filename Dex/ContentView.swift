4888a9abce94ed4392bc80432f9fe8eedeeaea6c
Mikhaeli <Mihaelirekhviashvili@gmail.com>
2026-03-22 20:44:35 +0400

    fetch service and storing

--- Dex/ContentView.swift
+++ Dex/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
         animation: .default)
     private var pokedex: FetchedResults<Pokemon>
 
+    let fetcher = FetchService()
+
     var body: some View {
         NavigationView {
             List {
@@ -34,8 +36,36 @@ struct ContentView: View {
                 }
                 ToolbarItem {
                     Button("Add Item", systemImage: "plus") {
-
+                        getPokemon()
                     }
+                }
+            }
+        }
+    }
+
+    private func getPokemon() {
+        Task {
+            for id in 1..<152 {
+                do {
+                    let fetchedPokemon = try await fetcher.fetchPokemon(id)
+                    let pokemon = Pokemon(context: viewContext)
+                    
+                    pokemon.id = fetchedPokemon.id
+                    pokemon.name = fetchedPokemon.name
+                    pokemon.types = fetchedPokemon.types
+                    pokemon.hp = fetchedPokemon.hp
+                    pokemon.attack = fetchedPokemon.attack
+                    pokemon.defense = fetchedPokemon.defense
+                    pokemon.specialAttack = fetchedPokemon.specialAttack
+                    pokemon.specialDefence = fetchedPokemon.specialDefense
+                    pokemon.speed = fetchedPokemon.speed
+                    pokemon.sprite = fetchedPokemon.sprite
+                    pokemon.shiny = fetchedPokemon.shiny
+                    
+                    try viewContext.save()
+                    
+                } catch {
+                    print(error)
                 }
             }
         }
